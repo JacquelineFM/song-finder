ab700d921930c1a6465ee098d631aa25f7b6e0cc
JacquelineFM <jfloresm.dev@gmail.com>
2022-10-07 22:52:42 -0500

    feat: add form structure and design

--- src/components/AppLyrics.jsx
+++ src/components/AppLyrics.jsx
@@ -1,9 +1,22 @@
+import Form from "./Form";
+
 const AppLyrics = () => {
   return (
-    <>
-      <header>Song lyrics search</header>
-      <main></main>
-    </>
+    <div className="w-full">
+      <header className="bg-black w-full text-center mx-auto py-10 px-4 sm:px-6 lg:py-14 lg:px-8">
+        <h2 className="text-3xl font-extrabold text-black sm:text-4xl">
+          <span className="block text-white">
+            Song lyrics <span className="text-emerald-500">search</span>
+          </span>
+        </h2>
+      </header>
+      <div className="grid grid-flow-row md:gap-4 md:grid-cols-3 lg:gap-20 lg:grid-cols-3 lg:mx-24 mx-6">
+        <div className="bg-white rounded-lg shadow w-full mx-auto p-8 my-8 lg:my-12 border-t-4 border-emerald-500 h-fit">
+          <Form />
+        </div>
+        <main className="bg-white rounded-lg shadow w-full mx-auto p-8 my-8 md:col-span-2 lg:my-12 lg:col-span-2 border-t-4 border-emerald-500"></main>
+      </div>
+    </div>
   );
 };
 
---
+++ src/components/Form.jsx
@@ -0,0 +1,57 @@
+import { useState } from "react";
+
+const Form = () => {
+  const [search, setSearch] = useState({ artist: "", song: "" });
+
+  return (
+    <div className="w-full mx-auto space-y-10">
+      <div className="space-y-6">
+        <div className="flex flex-col justify-between space-y-3">
+          <label htmlFor="artist" className="text-gray-700 font-bold uppercase">
+            Artist
+          </label>
+          <input
+            id="artist"
+            type="text"
+            name="artist"
+            className="rounded-lg border-transparent appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-emerald-400 focus:border-transparent"
+            placeholder="Camila Cabello"
+            value={search.artist}
+            onChange={(e) =>
+              setSearch({
+                ...search,
+                [e.target.name]: e.target.value,
+              })
+            }
+          />
+        </div>
+        <div className="flex flex-col justify-between space-y-3">
+          <label htmlFor="song" className="text-gray-700 font-bold uppercase">
+            Song
+          </label>
+          <input
+            id="song"
+            type="text"
+            name="song"
+            className="rounded-lg border-transparent appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-emerald-400 focus:border-transparent"
+            placeholder="Celia"
+            value={search.song}
+            onChange={(e) =>
+              setSearch({
+                ...search,
+                [e.target.name]: e.target.value,
+              })
+            }
+          />
+        </div>
+      </div>
+      <input
+        type="submit"
+        value="Search"
+        className="py-2 px-4 bg-emerald-500 hover:bg-emerald-600 focus:ring-emerald-400 focus:ring-offset-emerald-100 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2 rounded-lg cursor-pointer"
+      />
+    </div>
+  );
+};
+
+export default Form;
